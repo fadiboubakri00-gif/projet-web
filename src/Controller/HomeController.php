4b53ee3931c4214293712162292678c64b3a0108
Aziz Ben Jboura <aziz@example.com>
2025-11-08 14:56:05 +0100

    fazet nwamer

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,11 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
+use App\Repository\ProductRepository;
+use App\Repository\OrderRepository;
+use App\Repository\CartRepository;
+use App\Repository\CategoryRepository; // Add this
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +14,20 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(
+        UserRepository $userRepo,
+        ProductRepository $productRepo,
+        OrderRepository $orderRepo,
+        CartRepository $cartRepo,
+        CategoryRepository $categoryRepo // Add this
+    ): Response
     {
-        return $this->render('home/index.html.twig');
+        return $this->render('home/index.html.twig', [
+            'userCount' => $userRepo->count([]),
+            'productCount' => $productRepo->count([]),
+            'categoryCount' => $categoryRepo->count([]),
+            'cartCount' => $cartRepo->count([]),
+            'orderCount' => $orderRepo->count([]),
+        ]);
     }
 }
