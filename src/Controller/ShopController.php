0dff39f03a93601f28771bb0fb064191d6aa4287
unknown <mehdichrif0@gmail.com>
2025-11-14 17:49:00 +0100

    creation of the shop interface with the view of the product list

---
+++ src/Controller/ShopController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ShopController extends AbstractController
+{
+    #[Route('/shop', name: 'app_shop_home')]
+    public function index(ProductRepository $productRepository): Response
+    {
+        $products = $productRepository->findAll();
+
+        return $this->render('shop/index.html.twig', [
+            'products' => $products,
+        ]);
+    }
+}
